9fac4eee694e6aa4ae64f94e3aef8407684d1efb
aiellie <292223168+aiellie@users.noreply.github.com>
2026-08-13 13:02:33 -0400

    chore: move local dev to 3010, swap avatar initials and demo image

--- app/components/nav-user.tsx
+++ app/components/nav-user.tsx
@@ -46,7 +46,7 @@ export function NavUser({
           >
             <Avatar className="size-8 rounded-lg">
               <AvatarImage src={user.avatar} alt={user.name} />
-              <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              <AvatarFallback className="rounded-lg">AE</AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-start text-sm leading-tight">
               <span className="truncate font-medium">{user.name}</span>
